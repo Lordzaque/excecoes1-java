395adf2b61d1c818cf98391b54b6668be32decb5
Lordzaque <isaque.hsantos08@gmail.com>
2022-05-02 16:47:59 -0300

    Solução boa

--- src/aplicação/Programa.java
+++ src/aplicação/Programa.java
@@ -6,24 +6,23 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reserva;
+import model.exceptions.DomainException;
 
 public class Programa {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.print("Numero do quarto: ");
-		int numero = sc.nextInt();
-		System.out.print("Check-in data (dd/MM/yyy): ");
-		Date checkIn = sdf.parse(sc.next());
-		System.out.print("Check-out data (dd/MM/yyy): ");
-		Date checkOut = sdf.parse(sc.next());
+		try {
+			System.out.print("Numero do quarto: ");
+			int numero = sc.nextInt();
+			System.out.print("Check-in data (dd/MM/yyy): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out data (dd/MM/yyy): ");
+			Date checkOut = sdf.parse(sc.next());
 
-		if (!checkOut.after(checkIn)) {
-			System.out.println("Erro na reserva: A data de check-out deve ser posterior à data de check-in");
-		} else {
 			Reserva reserva = new Reserva(numero, checkIn, checkOut);
 			System.out.println("Reserva: " + reserva);
 
@@ -34,15 +33,18 @@ public class Programa {
 			System.out.print("Check-out data (dd/MM/yyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			String error = reserva.updateDates(checkIn, checkOut);
-			if (error != null) {
-				System.out.println("Erro na reserva: " + error);
-			} else {
-				System.out.println("Reserva: " + reserva);
-			}
-
+			reserva.updateDates(checkIn, checkOut);
+			System.out.println("Reserva: " + reserva);
+		} catch (ParseException e) {
+			System.out.println("Data invalida");
+		} catch (DomainException e) {
+			System.out.println("Erro na resarva: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Erro inesperado");
 		}
 
 		sc.close();
 	}
+
 }
